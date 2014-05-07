7171bee9e17db717b024eb6cbfc95a4712bc2389
Alexey Fedorov <waterlink000@gmail.com>
2014-05-07 14:41:24 +0300

    [reservation model] extracted overbooked method

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,10 +2,7 @@ class Reservation < ActiveRecord::Base
 
   validate :overbooking
 
-  private
-
-  def overbooking
-    # TODO Extract this to scope, so we can show them to user
+  def overbooked
     conditions = [
       '(start_at > :start_at and start_at < :end_at)',
       '(end_at > :start_at and end_at < :end_at)',
@@ -13,7 +10,13 @@ class Reservation < ActiveRecord::Base
       '(:end_at > start_at and :end_at < end_at)'
     ].join(' or ')
     conditions = "(#{conditions}) and \"table\" = :table"
-    if self.class.where(conditions, self.attributes.symbolize_keys).count > 0
+    self.class.where(conditions, self.attributes.symbolize_keys)
+  end
+
+  private
+
+  def overbooking
+    if overbooked.count > 0
       self.errors.add(:start_at, I18n.t(:overbooking))
       self.errors.add(:end_at, I18n.t(:overbooking))
     end
